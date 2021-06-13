d35e37b601b1758d9026be7567412b247c97784b
Andy Ye <ye.andyy@gmail.com>
2021-06-12 23:51:24 -0400

    Added form elements and a toggle button for read/not read

--- script.js
+++ script.js
@@ -1,4 +1,121 @@
 const addBook = document.getElementById('addbook');
+
+addBook.addEventListener('click', () => {
+    createForm();
+});
+
+
+function createForm(){
+    const form = document.querySelector('form');
+
+    let title = createTextInput('title');
+
+    let author = createTextInput('author');
+
+    let pages = createTextInput('pages');
+
+    let readToggle = createToggleSwitch();
+
+    form.append(title[0]);
+    form.append(document.createElement("br"));
+    form.append(title[1]);
+    form.append(document.createElement("br"));
+    form.append(document.createElement("br"));
+    
+    form.appendChild(author[0]);
+    form.append(document.createElement("br"));
+    form.appendChild(author[1]);
+    form.append(document.createElement("br"));
+    form.append(document.createElement("br"));
+
+    form.appendChild(pages[0]);
+    form.append(document.createElement("br"));
+    form.appendChild(pages[1]);
+    form.append(document.createElement("br"));
+    form.append(document.createElement("br"));
+
+    form.appendChild(readToggle);
+
+    const toggle = document.querySelector('.toggle input');
+
+    toggle.addEventListener('click', () => {
+        const onOff = toggle.parentNode.querySelector('.onoff');
+        onOff.textContent = toggle.checked ? 'Read' : 'Not read';
+    });
+}
+
+function createToggleSwitch(){
+
+    /*
+    <label class="toggle">
+      <span class="onoff">OFF</span>
+      <input type="checkbox"/>
+      <span class="slider round"></span>
+    </label>
+    */
+
+    const label = document.createElement('label');
+    label.classList.add('toggle');
+
+    const spanText = document.createElement('span');
+    spanText.classList.add('onoff');
+    spanText.textContent = "Not read";
+
+    const input = document.createElement('input');
+    input.setAttribute('type', 'checkbox');
+
+    const spanSlider = document.createElement('span');
+    spanSlider.classList.add('slider');
+    spanSlider.classList.add('round');
+    
+    label.appendChild(spanText);
+    label.append(input);
+    label.appendChild(spanSlider);
+
+    return label;
+}
+
+function createTextInput(str){
+    let tempStr = str.toLowerCase();
+    let tempArr = tempStr.split('');
+    tempArr[0] = tempArr[0].toUpperCase();
+    let capitalized = tempArr.join('');
+
+    const label = document.createElement('label');
+    label.setAttribute('for', str);
+    label.textContent = capitalized;
+
+    const input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', str);
+    input.setAttribute('name', str);
+
+    return [label, input];
+}
+
+function createLineBreaks(num){
+    let arr = [];
+    for (let i=0; i<num; i++){
+        let linebreak = document.createElement('label');
+        arr.push(linebreak);
+    }
+    return arr;
+}
+
+function changeSwitchTextContent(div){
+    console.log(div.textContent);
+    if (div.textContent==='Not read'){
+        div.textContent='Read';
+        return;
+    }
+    else {
+        div.textContent='Not read';
+        return;
+    }
+}
+
+console.log(createLineBreaks(4));
+
 let myLib = [];
 
 const books = [
